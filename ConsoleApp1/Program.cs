5a7ec1bacbd266d21cf862c397a157f446068c7c
Stas Vitkovskiy <Stasvitkovskiy700@gmail.com>
2017-09-20 23:02:11 +0300

    Исправили расчёт объёма и массы

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -24,15 +24,14 @@ namespace ConsoleApp1
                 FileStream file1 = new FileStream("test.txt", FileMode.Create);
                 StreamWriter writer = new StreamWriter(file1);
                 reader.BaseStream.Seek(84,SeekOrigin.Begin);
-                //number = reader.ReadUInt32();
                 while(reader.BaseStream.Position != reader.BaseStream.Length)
                 {
-                        for (int i = 0; i < 3; i++) normal[i] = reader.ReadSingle() * 10;
-                        for (int i = 0; i < 3; i++) vertex1[i] = reader.ReadSingle() * 0.01;
-                        for (int i = 0; i < 3; i++) vertex2[i] = reader.ReadSingle() * 0.01;
-                        for (int i = 0; i < 3; i++) vertex3[i] = reader.ReadSingle() * 0.01;
+                        for (int i = 0; i < 3; i++) normal[i] = reader.ReadSingle();
+                        for (int i = 0; i < 3; i++) vertex1[i] = reader.ReadSingle();
+                        for (int i = 0; i < 3; i++) vertex2[i] = reader.ReadSingle();
+                        for (int i = 0; i < 3; i++) vertex3[i] = reader.ReadSingle();
                         count = reader.ReadUInt16();
-                        sum =+ ((-1) * vertex3[0] * vertex2[1] * vertex1[2] + vertex2[0] * vertex3[1] * vertex1[2] + vertex3[0] * vertex1[1] * vertex2[2] - vertex1[0] * vertex3[1] * vertex2[2] - vertex2[0] * vertex1[1] * vertex3[2] + vertex1[0] * vertex2[1] * vertex3[2]) / 6;
+                        sum = sum + ((((-1) * vertex3[0] * vertex2[1] * vertex1[2]) + vertex2[0] * vertex3[1] * vertex1[2] + vertex3[0] * vertex1[1] * vertex2[2] - vertex1[0] * vertex3[1] * vertex2[2] - vertex2[0] * vertex1[1] * vertex3[2] + vertex1[0] * vertex2[1] * vertex3[2]) / 6);
                         writer.WriteLine("  facet normal " + normal[0] + " " + normal[1] + " " + normal[2]);
                         writer.WriteLine("    outer loop");
                         writer.WriteLine("      vertex " + vertex1[0] + " " + vertex1[1] + " " + vertex1[2]);
@@ -41,8 +40,9 @@ namespace ConsoleApp1
                         writer.WriteLine("    endloop");
                         writer.WriteLine("  endfacet");
                 }
-                
-                Console.WriteLine(sum*1.25);
+                sum = sum / 1000;
+                Console.WriteLine("Объём модели: " + sum + " см^3");
+                Console.WriteLine("Вес модели: " + (sum*1.25) + " грм");
                 Console.ReadLine();
                 writer.Close();
             }
